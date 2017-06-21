1db74aca0e0de0d5ac42010914059b5e0e4ecd34
Debrena McEwen <debrenamcewen@gmail.com>
2017-06-20 22:09:38 -0700

    WIP weekday

--- part-1/functions.js
+++ part-1/functions.js
@@ -1,1 +1,13 @@
+export default class Weekday {
+  constructor( year, month, day ) {
+    this.year = year;
+    this.month = month
+    this.day = day;
+  }
 
+  dayOfWeek(year, month, day) {
+    var date = new Date(year, month, day)
+    var day = date.getDay()
+    return day
+  }
+}
